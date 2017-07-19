500d071175d7a982fc6b2a7fe368cc1b25735f77
j-Sut <flowstategame@gmail.com>
2017-07-19 14:16:44 -0700

    Learned about props by getting new game to work with Wences

--- src/App.js
+++ src/App.js
@@ -5,13 +5,72 @@ import Header from './components/header'
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentGameTemp: 0,
+      currentGuess: 0,
+      pastGuesses: [],
+      feedback: ''
+    }
+  }
+
+  componentDidMount() {
+    this.setNewGame()
+  }
+
+  componentDidUpdate(){
+    console.log(this.state);
+  }
+
+  checkGuess() {
+    if (this.state.currentGuess === this.state.currentGameTemp) {
+      this.setState({feedback: 'You got it!'});
+    } else if (this.state.currentGuess < this.state.currentGameTemp){
+      this.setState({feedback: 'Too Cold'});
+    } else if (this.state.currentGuess > this.state.currentGameTemp){
+      this.setState({feedback: 'Too Hot'});
+    }
+  }
+
+  handleGuess(e) {
+    console.log(typeof(this.state.currentGuess));
+
+    let newStateArray = this.state.pastGuesses.slice();
+    newStateArray.push(this.state.currentGuess);
+    this.setState({pastGuesses: newStateArray});
+
+    this.checkGuess()
+  }
+
+  setGuess(e){
+    this.setState({currentGuess: parseInt(e.target.value)})
+  }
+
+  setNewGame(){
+    let newTemp = Math.floor(Math.random()*100)+1;
+
+    this.setState({
+      currentGameTemp: newTemp,
+      currentGuess: 0,
+      pastGuesses: [],
+      feedback: ''
+    })
+  }
+
 
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header newGame={ () => this.setNewGame() } />
         <h2 className="App-intro"> HOT or COLD </h2>
-        <HotColdGame />
+        <HotColdGame
+          pastGuesses={this.state.pastGuesses}
+          currentGuess={this.state.currentGuess}
+          handleGuess={() => this.handleGuess()}
+          setGuess={ e => this.setGuess(e)}
+          feedback={this.state.feedback}
+        />
       </div>
     );
   }
--- src/components/hot-cold.js
+++ src/components/hot-cold.js
@@ -2,54 +2,12 @@ import React from 'react';
 import './hot-cold.css'
 
 class Game extends React.Component{
-  constructor(props) {
+  constructor(props){
     super(props);
-    this.state = {
-      currentGameTemp: 0,
-      currentGuess: 0,
-      pastGuesses: [],
-      feedback: 'this is the feedback spot'
-    }
-  }
-
-
-
-  componentDidMount() {
-    let newTemp = Math.floor(Math.random()*100)+1;
-    this.setState({currentGameTemp:newTemp})
-  }
-
-
-  handleGuess(e) {
-    let newStateArray = this.state.pastGuesses.slice();
-    newStateArray.push(this.state.currentGuess);
-    this.setState({pastGuesses: newStateArray});
-
-    let thisGuess = this.state.currentGuess;
-    console.log('thisguess: ', thisGuess)
-    console.log("Game Temp: ", this.state.currentGameTemp);
-    console.log("Guess temp: ", this.state.currentGuess);
-
-    if (this.state.currentGuess == this.state.currentGameTemp) {
-      this.setState({feedback: 'You got it!'});
-    } else if (this.state.currentGuess < this.state.currentGameTemp){
-      this.setState({feedback: 'Too Cold!'});
-
-    } else if (this.state.currentGuess > this.state.currentGameTemp){
-      this.setState({feedback: 'Too Hot!'});
-
-    }
-
-  }
-
-
-
-  setGuess(e){
-    this.setState({currentGuess: e.target.value})
   }
 
   render() {
-    const guesses = this.state.pastGuesses.map((guess, index) =>
+    const guesses = this.props.pastGuesses.map((guess, index) =>
       <li key={index}> {guess} </li>
     );
 
@@ -59,16 +17,15 @@ class Game extends React.Component{
         <h3>Make your Guess!</h3>
         <section className='gameControls'>
           <input type='number' placeholder="Enter your Guess"
-          onChange={this.setGuess.bind(this)}
-          value={this.state.currentGuess}/> <br />
-          <button onClick={this.handleGuess.bind(this)}>Guess</button>
-
-          <p className='guessCount'> Guess #: {this.state.pastGuesses.length} </p>
+          onChange={this.props.setGuess}
+          value={this.props.currentGuess}/> <br />
+          <button onClick={this.props.handleGuess}>Guess</button>
+          <p className='guessCount'> Guess #: {this.props.pastGuesses.length} </p>
         </section>
         <div className='pastGuesses'>
           {guesses} <br /> <br />
         </div>
-        <section id='feedback'> {this.state.feedback} </section>
+        <section id='feedback'> {this.props.feedback} </section>
       </div>
     )
   }
